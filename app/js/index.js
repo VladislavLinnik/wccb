f2d84955f5725f53661b30151857937383801fa2
Vlad Linnik <vladislav.linnik.1999@gmail.com>
2019-03-06 17:47:16 +0200

    Изменения в офисе

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,30 @@
 "use strict"
 
+
+
+var googleMap;
+
+function initMap() {
+
+    // В переменной map создаем объект карты GoogleMaps и вешаем эту переменную на <div id="map"></div>
+    googleMap = new google.maps.Map(document.getElementById('map'), {
+        // Убираем навигацию
+        disableDefaultUI: true, 
+        // center - определяем точку на которой карта будет центрироваться
+        center: {lat: -34.397, lng: 150.644},
+        // zoom - определяет масштаб. 0 - видно всю платнеу. 18 - видно дома и улицы города.
+        zoom: 16
+    });
+}
+
+(function($) {
+    $(function() {
+ 
+        $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+            $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+ 
+    });
+})(jQuery);
