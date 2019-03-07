db0ff6f7abacf502b93d0dfec524101850aaec2c
Vlad Linnik <vladislav.linnik.1999@gmail.com>
2019-03-07 13:05:55 +0200

    Изменения в офисе

--- app/js/index.js
+++ app/js/index.js
@@ -1,23 +1,14 @@
-    (function($) {
-    $(function() {
+$(".tabs").tabs();
+$(".tarif-tabs").tabs();
+
+// (function($) {
+//     $(function() {
  
-        $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-            $(this)
-            .addClass('active').siblings().removeClass('active')
-            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-        });
+//         $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+//             $(this)
+//             .addClass('active').siblings().removeClass('active')
+//             .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+//         });
  
-    });
-})(jQuery);
-
-var googleMap;
-
-function initMap() {
-
-   googleMap = new google.maps.Map(document.getElementById('map'), {
-        // disable ui navigation
-        disableDefaultUI: true, 
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 16
-    });
-}
+//     });
+// })(jQuery);
